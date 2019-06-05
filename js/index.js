4c4ea27a99804c0e96ed6304ff5429055efec290
Motyar <dharmmotyar@gmail.com>
2019-06-05 08:22:47 +0000

    changed background

--- js/index.js
+++ js/index.js
@@ -38,22 +38,22 @@ Matrix44.createIdentity = function () {
 Matrix44.loadProjection = function (m, aspect, vdeg, near, far) {
     var h = near * Math.tan(vdeg * Math.PI / 180.0 * 0.5) * 2.0;
     var w = h * aspect;
-    
+
     m[0] = 2.0 * near / w;
     m[1] = 0.0;
     m[2] = 0.0;
     m[3] = 0.0;
-    
+
     m[4] = 0.0;
     m[5] = 2.0 * near / h;
     m[6] = 0.0;
     m[7] = 0.0;
-    
+
     m[8] = 0.0;
     m[9] = 0.0;
     m[10] = -(far + near) / (far - near);
     m[11] = -1.0;
-    
+
     m[12] = 0.0;
     m[13] = 0.0;
     m[14] = -2.0 * far * near / (far - near);
@@ -68,22 +68,22 @@ Matrix44.loadLookAt = function (m, vpos, vlook, vup) {
     var topv = Vector3.create(1.0, 0.0, 0.0);
     Vector3.cross(topv, frontv, sidev);
     Vector3.normalize(topv);
-    
+
     m[0] = sidev.x;
     m[1] = topv.x;
     m[2] = frontv.x;
     m[3] = 0.0;
-    
+
     m[4] = sidev.y;
     m[5] = topv.y;
     m[6] = frontv.y;
     m[7] = 0.0;
-    
+
     m[8] = sidev.z;
     m[9] = topv.z;
     m[10] = frontv.z;
     m[11] = 0.0;
-    
+
     m[12] = -(vpos.x * m[0] + vpos.y * m[4] + vpos.z * m[8]);
     m[13] = -(vpos.x * m[1] + vpos.y * m[5] + vpos.z * m[9]);
     m[14] = -(vpos.x * m[2] + vpos.y * m[6] + vpos.z * m[10]);
@@ -117,7 +117,7 @@ renderSpec.setSize = function(w, h) {
     renderSpec.array[0] = renderSpec.width;
     renderSpec.array[1] = renderSpec.height;
     renderSpec.array[2] = renderSpec.aspect;
-    
+
     renderSpec.halfWidth = Math.floor(w / 2);
     renderSpec.halfHeight = Math.floor(h / 2);
     renderSpec.halfArray[0] = renderSpec.halfWidth;
@@ -141,34 +141,34 @@ function createRenderTarget(w, h) {
     ret.frameBuffer = gl.createFramebuffer();
     ret.renderBuffer = gl.createRenderbuffer();
     ret.texture = gl.createTexture();
-    
+
     gl.bindTexture(gl.TEXTURE_2D, ret.texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    
+
     gl.bindFramebuffer(gl.FRAMEBUFFER, ret.frameBuffer);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, ret.texture, 0);
-    
+
     gl.bindRenderbuffer(gl.RENDERBUFFER, ret.renderBuffer);
     gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, w, h);
     gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, ret.renderBuffer);
-    
+
     gl.bindTexture(gl.TEXTURE_2D, null);
     gl.bindRenderbuffer(gl.RENDERBUFFER, null);
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    
+
     return ret;
 }
 
 function compileShader(shtype, shsrc) {
 	var retsh = gl.createShader(shtype);
-	
+
 	gl.shaderSource(retsh, shsrc);
 	gl.compileShader(retsh);
-	
+
 	if(!gl.getShaderParameter(retsh, gl.COMPILE_STATUS)) {
 		var errlog = gl.getShaderInfoLog(retsh);
 		gl.deleteShader(retsh);
@@ -181,32 +181,32 @@ function compileShader(shtype, shsrc) {
 function createShader(vtxsrc, frgsrc, uniformlist, attrlist) {
     var vsh = compileShader(gl.VERTEX_SHADER, vtxsrc);
     var fsh = compileShader(gl.FRAGMENT_SHADER, frgsrc);
-    
+
     if(vsh == null || fsh == null) {
         return null;
     }
-    
+
     var prog = gl.createProgram();
     gl.attachShader(prog, vsh);
     gl.attachShader(prog, fsh);
-    
+
     gl.deleteShader(vsh);
     gl.deleteShader(fsh);
-    
+
     gl.linkProgram(prog);
     if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
         var errlog = gl.getProgramInfoLog(prog);
         console.error(errlog);
         return null;
     }
-    
+
     if(uniformlist) {
         prog.uniforms = {};
         for(var i = 0; i < uniformlist.length; i++) {
             prog.uniforms[uniformlist[i]] = gl.getUniformLocation(prog, uniformlist[i]);
         }
     }
-    
+
     if(attrlist) {
         prog.attributes = {};
         for(var i = 0; i < attrlist.length; i++) {
@@ -214,7 +214,7 @@ function createShader(vtxsrc, frgsrc, uniformlist, attrlist) {
             prog.attributes[attr] = gl.getAttribLocation(prog, attr);
         }
     }
-    
+
     return prog;
 }
 
@@ -292,7 +292,7 @@ BlossomParticle.prototype.update = function (dt, et) {
     this.position[0] += this.velocity[0] * dt;
     this.position[1] += this.velocity[1] * dt;
     this.position[2] += this.velocity[2] * dt;
-    
+
     this.euler[0] += this.rotation[0] * dt;
     this.euler[1] += this.rotation[1] * dt;
     this.euler[2] += this.rotation[2] * dt;
@@ -302,20 +302,20 @@ function createPointFlowers() {
     // get point sizes
     var prm = gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE);
     renderSpec.pointSize = {'min':prm[0], 'max':prm[1]};
-    
+
     var vtxsrc = document.getElementById("sakura_point_vsh").textContent;
     var frgsrc = document.getElementById("sakura_point_fsh").textContent;
-    
+
     pointFlower.program = createShader(
         vtxsrc, frgsrc,
         ['uProjection', 'uModelview', 'uResolution', 'uOffset', 'uDOF', 'uFade'],
         ['aPosition', 'aEuler', 'aMisc']
     );
-    
+
     useShader(pointFlower.program);
     pointFlower.offset = new Float32Array([0.0, 0.0, 0.0]);
     pointFlower.fader = Vector3.create(0.0, 10.0, 0.0);
-    
+
     // paramerters: velocity[3], rotate[3]
     pointFlower.numFlowers = 1600;
     pointFlower.particles = new Array(pointFlower.numFlowers);
@@ -324,14 +324,14 @@ function createPointFlowers() {
     pointFlower.positionArrayOffset = 0;
     pointFlower.eulerArrayOffset = pointFlower.numFlowers * 3;
     pointFlower.miscArrayOffset = pointFlower.numFlowers * 6;
-    
+
     pointFlower.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pointFlower.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, pointFlower.dataArray, gl.DYNAMIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    
+
     unuseShader(pointFlower.program);
-    
+
     for(var i = 0; i < pointFlower.numFlowers; i++) {
         pointFlower.particles[i] = new BlossomParticle();
     }
@@ -341,11 +341,11 @@ function initPointFlowers() {
     //area
     pointFlower.area = Vector3.create(20.0, 20.0, 20.0);
     pointFlower.area.x = pointFlower.area.y * renderSpec.aspect;
-    
+
     pointFlower.fader.x = 10.0; //env fade start
     pointFlower.fader.y = pointFlower.area.z; //env fade half
     pointFlower.fader.z = 0.1;  //near fade start
-    
+
     //particles
     var PI2 = Math.PI * 2.0;
     var tmpv3 = Vector3.create(0, 0, 0);
@@ -353,7 +353,7 @@ function initPointFlowers() {
     var symmetryrand = function() {return (Math.random() * 2.0 - 1.0);};
     for(var i = 0; i < pointFlower.numFlowers; i++) {
         var tmpprtcl = pointFlower.particles[i];
-        
+
         //velocity
         tmpv3.x = symmetryrand() * 0.3 + 0.8;
         tmpv3.y = symmetryrand() * 0.2 - 1.0;
@@ -361,28 +361,28 @@ function initPointFlowers() {
         Vector3.normalize(tmpv3);
         tmpv = 2.0 + Math.random() * 1.0;
         tmpprtcl.setVelocity(tmpv3.x * tmpv, tmpv3.y * tmpv, tmpv3.z * tmpv);
-        
+
         //rotation
         tmpprtcl.setRotation(
             symmetryrand() * PI2 * 0.5,
             symmetryrand() * PI2 * 0.5,
             symmetryrand() * PI2 * 0.5
         );
-        
+
         //position
         tmpprtcl.setPosition(
             symmetryrand() * pointFlower.area.x,
             symmetryrand() * pointFlower.area.y,
             symmetryrand() * pointFlower.area.z
         );
-        
+
         //euler
         tmpprtcl.setEulerAngles(
             Math.random() * Math.PI * 2.0,
             Math.random() * Math.PI * 2.0,
             Math.random() * Math.PI * 2.0
         );
-        
+
         //size
         tmpprtcl.setSize(0.9 + Math.random() * 0.1);
     }
@@ -409,7 +409,7 @@ function renderPointFlowers() {
             prt.euler[cmp] += PI2;
         }
     };
-    
+
     for(var i = 0; i < pointFlower.numFlowers; i++) {
         var prtcl = pointFlower.particles[i];
         prtcl.update(timeInfo.delta, timeInfo.elapsed);
@@ -419,18 +419,18 @@ function renderPointFlowers() {
         repeatEuler(prtcl, 0);
         repeatEuler(prtcl, 1);
         repeatEuler(prtcl, 2);
-        
+
         prtcl.alpha = 1.0;//(pointFlower.area.z - prtcl.position[2]) * 0.5;
-        
+
         prtcl.zkey = (camera.matrix[2] * prtcl.position[0]
                     + camera.matrix[6] * prtcl.position[1]
                     + camera.matrix[10] * prtcl.position[2]
                     + camera.matrix[14]);
     }
-    
+
     // sort
     pointFlower.particles.sort(function(p0, p1){return p0.zkey - p1.zkey;});
-    
+
     // update data
     var ipos = pointFlower.positionArrayOffset;
     var ieuler = pointFlower.eulerArrayOffset;
@@ -449,28 +449,28 @@ function renderPointFlowers() {
         pointFlower.dataArray[imisc + 1] = prtcl.alpha;
         imisc += 2;
     }
-    
+
     //draw
     gl.enable(gl.BLEND);
     //gl.disable(gl.DEPTH_TEST);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    
+
     var prog = pointFlower.program;
     useShader(prog);
-    
+
     gl.uniformMatrix4fv(prog.uniforms.uProjection, false, projection.matrix);
     gl.uniformMatrix4fv(prog.uniforms.uModelview, false, camera.matrix);
     gl.uniform3fv(prog.uniforms.uResolution, renderSpec.array);
     gl.uniform3fv(prog.uniforms.uDOF, Vector3.arrayForm(camera.dof));
     gl.uniform3fv(prog.uniforms.uFade, Vector3.arrayForm(pointFlower.fader));
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, pointFlower.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, pointFlower.dataArray, gl.DYNAMIC_DRAW);
-    
+
     gl.vertexAttribPointer(prog.attributes.aPosition, 3, gl.FLOAT, false, 0, pointFlower.positionArrayOffset * Float32Array.BYTES_PER_ELEMENT);
     gl.vertexAttribPointer(prog.attributes.aEuler, 3, gl.FLOAT, false, 0, pointFlower.eulerArrayOffset * Float32Array.BYTES_PER_ELEMENT);
     gl.vertexAttribPointer(prog.attributes.aMisc, 2, gl.FLOAT, false, 0, pointFlower.miscArrayOffset * Float32Array.BYTES_PER_ELEMENT);
-    
+
     // doubler
     for(var i = 1; i < 2; i++) {
         var zpos = i * -2.0;
@@ -479,36 +479,36 @@ function renderPointFlowers() {
         pointFlower.offset[2] = pointFlower.area.z * zpos;
         gl.uniform3fv(prog.uniforms.uOffset, pointFlower.offset);
         gl.drawArrays(gl.POINT, 0, pointFlower.numFlowers);
-        
+
         pointFlower.offset[0] = pointFlower.area.x * -1.0;
         pointFlower.offset[1] = pointFlower.area.y *  1.0;
         pointFlower.offset[2] = pointFlower.area.z * zpos;
         gl.uniform3fv(prog.uniforms.uOffset, pointFlower.offset);
         gl.drawArrays(gl.POINT, 0, pointFlower.numFlowers);
-        
+
         pointFlower.offset[0] = pointFlower.area.x *  1.0;
         pointFlower.offset[1] = pointFlower.area.y * -1.0;
         pointFlower.offset[2] = pointFlower.area.z * zpos;
         gl.uniform3fv(prog.uniforms.uOffset, pointFlower.offset);
         gl.drawArrays(gl.POINT, 0, pointFlower.numFlowers);
-        
+
         pointFlower.offset[0] = pointFlower.area.x *  1.0;
         pointFlower.offset[1] = pointFlower.area.y *  1.0;
         pointFlower.offset[2] = pointFlower.area.z * zpos;
         gl.uniform3fv(prog.uniforms.uOffset, pointFlower.offset);
         gl.drawArrays(gl.POINT, 0, pointFlower.numFlowers);
     }
-    
+
     //main
     pointFlower.offset[0] = 0.0;
     pointFlower.offset[1] = 0.0;
     pointFlower.offset[2] = 0.0;
     gl.uniform3fv(prog.uniforms.uOffset, pointFlower.offset);
     gl.drawArrays(gl.POINT, 0, pointFlower.numFlowers);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     unuseShader(prog);
-    
+
     gl.enable(gl.DEPTH_TEST);
     gl.disable(gl.BLEND);
 }
@@ -525,10 +525,10 @@ function createEffectProgram(vtxsrc, frgsrc, exunifs, exattrs) {
     if(exattrs) {
         attrs = attrs.concat(exattrs);
     }
-    
+
     ret.program = createShader(vtxsrc, frgsrc, unifs, attrs);
     useShader(ret.program);
-    
+
     ret.dataArray = new Float32Array([
         -1.0, -1.0,
          1.0, -1.0,
@@ -538,10 +538,10 @@ function createEffectProgram(vtxsrc, frgsrc, exunifs, exattrs) {
     ret.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, ret.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, ret.dataArray, gl.STATIC_DRAW);
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     unuseShader(ret.program);
-    
+
     return ret;
 }
 
@@ -555,7 +555,7 @@ function useEffect(fxobj, srctex) {
     var prog = fxobj.program;
     useShader(prog);
     gl.uniform3fv(prog.uniforms.uResolution, renderSpec.array);
-    
+
     if(srctex != null) {
         gl.uniform1i(prog.uniforms.sampler0, 0);
 
@@ -574,23 +574,23 @@ function unuseEffect(fxobj) {
 
 var effectLib = {};
 function createEffectLib() {
-    
+
     var vtxsrc, frgsrc;
     //common
     var cmnvtxsrc = document.getElementById("fx_common_vsh").textContent;
-    
+
     //background
     frgsrc = document.getElementById("bg_fsh").textContent;
     effectLib.sceneBg = createEffectProgram(cmnvtxsrc, frgsrc, ['uTimes'], null);
-    
+
     // make brightpixels buffer
     frgsrc = document.getElementById("fx_brightbuf_fsh").textContent;
     effectLib.mkBrightBuf = createEffectProgram(cmnvtxsrc, frgsrc, null, null);
-    
+
     // direction blur
     frgsrc = document.getElementById("fx_dirblur_r4_fsh").textContent;
     effectLib.dirBlur = createEffectProgram(cmnvtxsrc, frgsrc, ['uBlurDir'], null);
-    
+
     //final composite
     vtxsrc = document.getElementById("pp_final_vsh").textContent;
     frgsrc = document.getElementById("pp_final_fsh").textContent;
@@ -637,7 +637,7 @@ function renderBackground() {
     gl.uniform2f(effectLib.sceneBg.program.uniforms.uTimes, timeInfo.elapsed, timeInfo.delta);
     drawEffect(effectLib.sceneBg);
     unuseEffect(effectLib.sceneBg);
-    
+
     gl.enable(gl.DEPTH_TEST);
 }
 
@@ -661,7 +661,7 @@ function renderPostProcess() {
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         }
     };
-    
+
     //make bright buff
 
     /*
@@ -680,26 +680,26 @@ function renderPostProcess() {
         gl.uniform4f(effectLib.dirBlur.program.uniforms.uBlurDir, p, 0.0, s, 0.0);
         drawEffect(effectLib.dirBlur);
         unuseEffect(effectLib.dirBlur);
-        
+
         bindRT(renderSpec.wHalfRT0, true);
         useEffect(effectLib.dirBlur, renderSpec.wHalfRT1);
         gl.uniform4f(effectLib.dirBlur.program.uniforms.uBlurDir, 0.0, p, 0.0, s);
         drawEffect(effectLib.dirBlur);
         unuseEffect(effectLib.dirBlur);
     }
-    
+
     //display
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.viewport(0, 0, renderSpec.width, renderSpec.height);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     useEffect(effectLib.finalComp, renderSpec.mainRT);
     gl.uniform1i(effectLib.finalComp.program.uniforms.uBloom, 1);
     gl.activeTexture(gl.TEXTURE1);
     gl.bindTexture(gl.TEXTURE_2D, renderSpec.wHalfRT0.texture);
     drawEffect(effectLib.finalComp);
     unuseEffect(effectLib.finalComp);
-    
+
     gl.enable(gl.DEPTH_TEST);
 }
 
@@ -717,7 +717,7 @@ function initScene() {
     initBackground();
     initPointFlowers();
     initPostProcess();
-    
+
     //camera.position.z = 17.320508;
     camera.position.z = pointFlower.area.z + projection.nearfar[0];
     projection.angle = Math.atan2(pointFlower.area.y, camera.position.z + pointFlower.area.z) * 180.0 / Math.PI * 2.0;
@@ -727,15 +727,15 @@ function initScene() {
 function renderScene() {
     //draw
     Matrix44.loadLookAt(camera.matrix, camera.position, camera.lookat, camera.up);
-    
+
     gl.enable(gl.DEPTH_TEST);
-    
+
     //gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.bindFramebuffer(gl.FRAMEBUFFER, renderSpec.mainRT.frameBuffer);
     gl.viewport(0, 0, renderSpec.mainRT.width, renderSpec.mainRT.height);
     gl.clearColor(0.005, 0, 0.05, 0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     renderBackground();
     renderPointFlowers();
     renderPostProcess();
@@ -752,10 +752,10 @@ function onResize(e) {
 
 function setViewports() {
     renderSpec.setSize(gl.canvas.width, gl.canvas.height);
-    
+
     gl.clearColor(0.2, 0.2, 0.5, 1.0);
     gl.viewport(0, 0, renderSpec.width, renderSpec.height);
-    
+
     var rtfunc = function (rtname, rtw, rth) {
         var rt = renderSpec[rtname];
         if(rt) deleteRenderTarget(rt);
@@ -815,7 +815,7 @@ function animate() {
     timeInfo.elapsed = (curdate - timeInfo.start) / 1000.0;
     timeInfo.delta = (curdate - timeInfo.prev) / 1000.0;
     timeInfo.prev = curdate;
-    
+
     if(animating) requestAnimationFrame(animate);
     render();
 }
@@ -839,13 +839,13 @@ window.addEventListener('load', function(e) {
         console.error(e);
         return;
     }
-    
+
     window.addEventListener('resize', onResize);
-    
+
     setViewports();
     createScene();
     initScene();
-    
+
     timeInfo.start = new Date();
     timeInfo.prev = timeInfo.start;
     animate();
